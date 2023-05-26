cf7f75ceae562498a81a9b9760288a0237c8d0c5
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2023-05-26 07:49:55 -0400

    Initial commit

---
+++ src/main/java/com/isimondev/parcelpulse/ParcelpulseApplication.java
@@ -0,0 +1,13 @@
+package com.isimondev.parcelpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelpulseApplication.class, args);
+	}
+
+}
